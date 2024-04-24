daa36355319a249a876867b5dd9d5f97551e18c4
Ripleonardo000 <leonardoandrade2003.197@gmail.com>
2024-04-24 10:52:07 -0500

    Creacion de la carpeta de datos y su aplicacion para los modelos commit two

---
+++ DatosLeonardoAndrade/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AndradeLeonardo_ExamenProgreso1.DatosLeonardoAndrade
+{
+    public class ApplicationDbContext : DbContext
+
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+
+    //AQUI VOY A PONER LOS MODELOS QUE CORRESPONDEN A UNA TABLA DE DATOS
+}
