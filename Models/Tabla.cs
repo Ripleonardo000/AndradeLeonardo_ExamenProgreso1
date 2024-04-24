deee3138b34e8df2e3aaf01620bb5e89aba19d00
Ripleonardo000 <leonardoandrade2003.197@gmail.com>
2024-04-24 11:02:47 -0500

    Creacion de los atributos en la clase Tabla de la carpeta models, con required, valores que se piden creados commit three

---
+++ Models/Tabla.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AndradeLeonardo_ExamenProgreso1.Models
+{
+    public class Tabla
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Es obligacion llenar el nombre")]
+        public String Nombre { get; set; }
+
+        [Required(ErrorMessage = "Es obligacion llenar el espacio")]
+        public float sueldo { get; set; }
+
+
+        [Required(ErrorMessage = "Es obligacion llenar el espacio")]
+        public bool Pregunta { get; set; }
+
+
+        public DateTime Fecha { get; set; }
+
+    }
+}
